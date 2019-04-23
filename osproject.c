6d6cef5a02cfff1a8fca4bb7784095c6624f9a67
Sk-1206 <49876972+Sk-1206@users.noreply.github.com>
2019-04-23 21:00:41 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -5,4 +5,14 @@ int main()
 {
 	char p[10][5],temp[5];
 	int i,j,pt[10],pr[10],temp1,n;
-	printf("Enter n
+	printf("Enter no of girls:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("enter girl %d name:",i+1);
+  		scanf("%s",&p[i]);
+		printf("enter process time:");
+		scanf("%d",&pt[i]);
+		printf("enter no of gifts:");
+		scanf("%d",&pr[i]);
+	}
