30124990067547507e0355712fa260ad50152b6f
Sk-1206 <49876972+Sk-1206@users.noreply.github.com>
2019-04-24 20:59:06 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -15,9 +15,30 @@ int main()
 		scanf("%d",&pt[i]);
 		printf("enter no of gifts:");
 		scanf("%d",&pr[i]);
-		
-		
-		
-		
-		
 	}
+for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(pr[i]<pr[j])
+			{
+				temp1=pr[i];
+				pr[i]=pr[j];
+				pr[j]=temp1;
+				temp1=pt[i];
+				pt[i]=pt[j];
+				pt[j]=temp1;
+				strcpy(temp,p[i]);
+				strcpy(p[i],p[j]);
+				strcpy(p[j],temp);
+			}
+		}
+	}
+	printf("G_name\t P_time\t No.Gifts\n");
+	for(i=0;i<n;i++)
+	{
+	   printf(" %s\t %d\t %d\t\n" ,p[i],pt[i],pr[i]);
+	}
+	getch();
+}
+
